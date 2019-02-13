ca99ae52a4a60b4a8c45b976b0ef5b293cae9830
McClannahan <WM065979@cerner.net>
2019-02-13 11:18:33 -0600

    Added log,map,map-options,personal-notes,shared-notes,combat components. Added basic routing

---
+++ src/app/personal-notes/personal-notes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-notes',
+  templateUrl: './personal-notes.component.html',
+  styleUrls: ['./personal-notes.component.css']
+})
+export class PersonalNotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
